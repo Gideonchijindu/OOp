fb980ddbc2a3688afddce0f46f37e98d8a5a767f
Gideon <gnwande@gmail.com>
2024-02-04 06:42:34 +0100

    initial commit

---
+++ Animal.js
@@ -0,0 +1,221 @@
+// Parent Class
+class Animal {
+  constructor(
+    name,
+    skeletalStructure,
+    bodyTemperature,
+    habitat,
+    lifespan,
+    prey
+  ) {
+    this.name = name;
+    this.skeletalStructure = skeletalStructure;
+    this.bodyTemperature = bodyTemperature;
+    this.habitat = habitat;
+    this.lifespan = lifespan;
+    this.prey = prey;
+  }
+
+  Alive = true;
+  Eat = "All Living Animal eats!";
+}
+
+// Inheritance/Child Class
+class Arthropoda extends Animal {
+  constructor(
+    name,
+    skeletalStructure,
+    bodyTemperature,
+    habitat,
+    lifespan,
+    prey
+  ) {
+    super(name, skeletalStructure, bodyTemperature, habitat, lifespan, prey);
+  }
+
+  static animalKingdom() {
+    return "Arthropoda";
+  }
+  wings() {
+    return `They have wings, e.g is ${this.name}`;
+  }
+  circulatorySystem() {
+    return `${this.name}s have an open circulatory system`;
+  }
+  sensoryOrgans() {
+    return `${this.name}s has sensory organs like hairs, antennae, simple and compound eyes, auditory organs, and statocyst`;
+  }
+}
+
+class Fish extends Animal {
+  constructor(
+    name,
+    skeletalStructure,
+    bodyTemperature,
+    habitat,
+    lifespan,
+    prey
+  ) {
+    super(name, skeletalStructure, bodyTemperature, habitat, lifespan, prey);
+  }
+
+  swim() {
+    return `${this.name} are ${this.bodyTemperature} and ${this.skeletalStructure}. They live in water and feed on inferior members of the population.`;
+  }
+}
+
+class Amphibia extends Animal {
+  constructor(
+    name,
+    skeletalStructure,
+    bodyTemperature,
+    habitat,
+    lifespan,
+    prey
+  ) {
+    super(name, skeletalStructure, bodyTemperature, habitat, lifespan, prey);
+  }
+
+  jump() {
+    return `${this.name}s have bones. They live both in ${this.habitat}.`;
+  }
+}
+
+class Reptiles extends Animal {
+  constructor(
+    name,
+    skeletalStructure,
+    bodyTemperature,
+    habitat,
+    lifespan,
+    prey
+  ) {
+    super(name, skeletalStructure, bodyTemperature, habitat, lifespan, prey);
+  }
+
+  territorial() {
+    return `${this.name} have ${this.skeletalStructure} and are ${this.bodyTemperature}, this makes them territorial.`;
+  }
+}
+
+class Aves extends Animal {
+  constructor(
+    name,
+    skeletalStructure,
+    bodyTemperature,
+    habitat,
+    lifespan,
+    prey,
+    wingspan
+  ) {
+    super(name, skeletalStructure, bodyTemperature, habitat, lifespan, prey);
+    this.wingspan = wingspan;
+  }
+
+  fly() {
+    return `A ${this.name} ${this.skeletalStructure} and ${this.wingspan} span of wings for flying.`;
+  }
+}
+
+class Mammal extends Animal {
+  constructor(
+    name,
+    skeletalStructure,
+    bodyTemperature,
+    habitat,
+    lifespan,
+    prey,
+    sound
+  ) {
+    super(name, skeletalStructure, bodyTemperature, habitat, lifespan, prey);
+    this.sound = sound;
+  }
+
+  claws() {
+    return `Most mammals have claws e.g is the ${this.name}. They mostly use it as a mode of defense and protection against predators.
+    `;
+  }
+  soundVoice() {
+    return `${this.name}s ${this.sound}.`;
+  }
+}
+
+// Instantiating the Child Classes
+const arthropoda = new Arthropoda(
+  "butterfly",
+  "with out bone",
+  "coldblooded",
+  "land and air",
+  "varies",
+  "vertebrates, spiders, snakes and possums"
+);
+const fish = new Fish(
+  "Whales",
+  "with bone",
+  "coldblooded",
+  "water",
+  "5 years",
+  "bony fishes, sea birds"
+);
+const amphibia = new Amphibia(
+  "Toad",
+  "with bone",
+  "coldblooded",
+  "water and land",
+  "twelve years",
+  "insects"
+);
+const reptiles = new Reptiles(
+  "tortoise",
+  "with bone",
+  "coldblooded",
+  "water and land",
+  "fifty years",
+  "fungi",
+  "vegetables"
+);
+const aves = new Aves(
+  "bird",
+  "with bone",
+  "warmblooded",
+  "air and land",
+  "fifty years",
+  "chicks",
+  "one hundred and twenty centimeters"
+);
+const mammal = new Mammal(
+  "Tiger",
+  "with bone",
+  "warmblooded",
+  "land",
+  "fourteen years",
+  "rat",
+  "meow"
+);
+
+console.log("(1)");
+console.log(arthropoda);
+console.log(arthropoda.wings());
+console.log(arthropoda.circulatorySystem());
+console.log(arthropoda.sensoryOrgans());
+
+console.log("(2)");
+console.log(fish);
+console.log(fish.swim());
+
+console.log("(3)");
+console.log(amphibia);
+console.log(amphibia.jump());
+
+console.log("(4)");
+console.log(reptiles);
+console.log(reptiles.territorial());
+
+console.log("(5)");
+console.log(aves);
+console.log(aves.fly());
+
+console.log("(6)");
+console.log(mammal);
+console.log(mammal.soundVoice());
+console.log(mammal.claws());
---
+++ README.md
@@ -0,0 +1,36 @@
+# OOP-AnimalKingdom_model
+
+## Overview
+
+This task aims to improve your understanding and application of Object-Oriented Programming (OOP) and Unified Modeling Language (UML) diagrams. The project involves implementing OOP principles in JavaScript to model the Animal Kingdom Classification Diagram.
+
+## Usage
+
+1. Clone the repository.
+2. Navigate to the 'OOP-Implementation-Animal-Kingdom' directory.
+3. Ensure Node.js is installed.
+4. Run the code using the provided terminal command.
+
+## Lessons Learned
+
+- Solid grasp of OOP principles: Inheritance, Abstraction, Encapsulation, and Polymorphism.
+- Extensive application of Object-Oriented Programming.
+- Proper utilization of Classes.
+
+## Important Notes
+
+- Object-Oriented Analysis (OOA) and Object-Oriented Design (OOD) emphasize the significance of detailed Use Case and Class Diagrams. Incorrect designs can lead to faulty code.
+
+## Contributing
+
+- Open for contributions.
+- Pull requests are welcome; discuss changes before implementation.
+- To contribute, fork the repository, add your scripts, commit, push, and create a pull request.
+
+## Show Your Support
+
+If you find this project helpful, give it a ⭐️!
+
+## License
+
+MIT
